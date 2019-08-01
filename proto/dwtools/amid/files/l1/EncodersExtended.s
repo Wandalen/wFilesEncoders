d6fd6119e291a7838694f36d25c3f7476683302c
Wandalen <wandalen.me@gmail.com>
2019-08-01 10:43:48 +0300

    auto commit

--- proto/dwtools/amid/files/l1/EncodersExtended.s
+++ proto/dwtools/amid/files/l1/EncodersExtended.s
@@ -1,6 +1,6 @@
 (function _EncodersExtended_s_() {
 
-'use strict'; 
+'use strict';
 
 /**
  * Collection of files transformers for Files module. Use it to read configs in different formats.
@@ -261,9 +261,9 @@ Object.assign( _.FileReadEncoders, FileReadEncoders );
 Object.assign( _.FileWriteEncoders, FileWriteEncoders );
 
 if( _.FileProvider && _.FileProvider.Partial && _.FileProvider.Partial.prototype.fileRead.encoders )
-_.assert( _.prototypeOf( _.FileReadEncoders, _.FileProvider.Partial.prototype.fileRead.encoders ) );
+_.assert( _.isPrototypeOf( _.FileReadEncoders, _.FileProvider.Partial.prototype.fileRead.encoders ) );
 if( _.FileProvider && _.FileProvider.Partial && _.FileProvider.Partial.prototype.fileWrite.encoders )
-_.assert( _.prototypeOf( _.FileWriteEncoders, _.FileProvider.Partial.prototype.fileWrite.encoders ) );
+_.assert( _.isPrototypeOf( _.FileWriteEncoders, _.FileProvider.Partial.prototype.fileWrite.encoders ) );
 
 // --
 // export
